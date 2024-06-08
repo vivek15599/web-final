4e06cfdffff54d4b73c875a0a4f962725a275edd
vivek15599 <155vivekpatel@gmail.com>
2024-06-09 01:09:59 +1000

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "JobRecruitment";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
